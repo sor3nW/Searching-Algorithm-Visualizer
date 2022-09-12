b65d445101045ebfd6e7ad0c8c4fe623dcc7140b
sor3nW <97264127+sor3nW@users.noreply.github.com>
2022-09-12 18:34:11 -0500

    Update BFS-pygame-visualization.py

--- BFS-pygame-visualization.py
+++ BFS-pygame-visualization.py
@@ -10,6 +10,7 @@ dimensions = screenWidth, screenHeight = 500,800
 screen = pygame.display.set_mode((dimensions))
 # this is the first commit from github
 # now Im making a change from my local repository
+# making sure this works
 num = 15
 grid = [[0 for i in range(num)]for j in range(num)]
 diff = screenWidth//num
